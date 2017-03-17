4e6e8962ec2955f9811d9f6202d5772b763eccf2
Abdurahman Gugssa (Wipro Ltd.) <v-abdgug@microsoft.com>
2017-03-17 15:50:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // Make a new express app
 // 'views' is the defalut directory express uses for your templates
